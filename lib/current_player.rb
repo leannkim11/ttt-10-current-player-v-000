ba01d8ff964292a4089b888de1275c98beb059b6
Leann Kim <leannminakim@gmail.com>
2018-03-11 01:05:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,10 @@
 def turn_count(board)
     board.each do |count_turn|
+    new_array =[]
       if count_turn == "X" || count_turn == "O"
-        new_array =[]
         new_array << count_turn
       end
+    new_array.count
     end
 end
 
