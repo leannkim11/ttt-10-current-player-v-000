3e724540d2d6e36ffbb2f71e1593d392356c3983
Leann Kim <leannminakim@gmail.com>
2018-03-11 00:33:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,10 @@ def turn_count(board)
   counter = 0
     board.each do |count|
       board_count = []
-      if "#{count}" == "X" || count == "O"
-        puts "#{count}".count
+      if count == "X" || count == "O"
+        puts count.count
       counter += 1
-      board_count << "#{count}"
+      board_count << count
       end
 end
 
