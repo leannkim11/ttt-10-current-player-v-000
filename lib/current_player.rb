51cb4424135f091b550de1d5115b95604ce9935a
Leann Kim <leannminakim@gmail.com>
2018-03-11 01:10:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,12 @@
-def turn_count(board)
-    board.each do |count_turn|
-    new_array =[]
-      if count_turn == "X" || count_turn == "O"
-        new_array << count_turn
-      end
-    new_array.count
-    end
-end
+# def turn_count(board)
+#     board.each do |count_turn|
+#     new_array =[]
+#       if count_turn == "X" || count_turn == "O"
+#         new_array << count_turn
+#       end
+#     new_array.count
+#     end
+# end
 
 def turn_count(board)
   counter = 0
